c42ded50e073a092de4f3fa32ee18dcc69ad1ce9
Rauqoz <raul4.antonio@yahoo.com>
2022-04-21 23:43:59 -0600

    squashed: backend

--- README.md
+++ README.md
@@ -5,14 +5,22 @@ Mono Repo
 ## Back End
 ### Integrations and Structures
 
- 1. Express + Cors
- 2. Mongoose fror MongoDB
- 3. Socket IO
- 4. Swagger Documentation
- 5. File for Deploy Mono Repos with Vercel
+    1. Express + Cors
+    2. Mongoose fror MongoDB
+    3. Socket IO
+    4. Swagger Documentation
+    5. File for Deploy Mono Repos with Vercel
 
 >Remember check the index file in "backend" folder
 
 You can start using:
 
     npm install
+
+And run the backend with nodemon
+
+    npm run dev
+
+    or
+
+    npm start
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,10 @@
 // Const of Server
 const server_on = require('./server/server');
 /* 
-    Module Socket IO
-    If you need use this please use "npm i socket.io"
-    and uncomment line "const io"
+    Check the folder "events"
 */
 // const io = require('./events/socket').listen(server_on);
 /* 
-    Module Mongoose
-    If you need use this please use "npm i mongoose"
-    and uncomment line "const mongo_on"
-    don't forget the file ".env" with MONGO_DB='your string connection'
+    Check the folder "mongodb"
 */
 // const mongo_on = require('./mongodb/mongo_on');
