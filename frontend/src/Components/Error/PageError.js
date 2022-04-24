e80c4503978f5a6fece7b78980ab93f03970e108
Rauqoz <raul4.antonio@yahoo.com>
2022-04-23 18:17:14 -0600

    feat: changes on routes

---
+++ frontend/src/Components/Error/PageError.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TemplateS } from '../Templates/Styles/Template.styles';
+
+const PageError = () => {
+	return (
+		<div>
+			<TemplateS>
+				<code>Page Error 404 :c</code>
+			</TemplateS>
+		</div>
+	);
+};
+
+export default PageError;
--- frontend/src/Components/Templates/Template.js
+++ frontend/src/Components/Templates/Template.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Outlet, useNavigate } from 'react-router';
+import { useNavigate, Outlet } from 'react-router';
 import { TemplateS } from './Styles/Template.styles';
 
 const Template = () => {
 	const navigate = useNavigate();
 
 	return (
-		<div>
-			<TemplateS>
-				<code>Rauqoz</code>
-				<button onClick={() => navigate('/child', { state: 'child for props' })}>Go to Child</button>
-				<Outlet />
-			</TemplateS>
-		</div>
+		<TemplateS>
+			<code>Rauqoz</code>
+			<button onClick={() => navigate('/child', { state: 'See the URL' })}>Go to Child</button>
+			<Outlet />
+		</TemplateS>
 	);
 };
 
